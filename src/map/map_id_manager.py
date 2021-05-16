3e38d728d8203f2c497f0a8ab3f14280ee647dae
LAPTOP-627O75T4\zhong Ming <www.snowbanana@hotmail.sg>
2021-05-16 16:25:03 +0800

    created MapIdManager class

--- src/map/map_id_manager.py
+++ src/map/map_id_manager.py
@@ -1,3 +1,14 @@
+import os
+import pygame
+import definitions
+
+root = definitions.ROOTDIR
 class MapIdManager:
     def __init__(self):
-        pass
+        self.id_texture_map = {}
+        self.id_texture_map["forest_fresh_grass"] = pygame.image.load(os.path.join(root, "assets\\map_tiles\\forest", "fresh_grass.png"))
+        self.id_texture_map["forest_short_grass"] = pygame.image.load(os.path.join(root, "assets\\map_tiles\\forest", "short_grass.png"))
+        self.id_texture_map["forest_thick_grass"] = pygame.image.load(os.path.join(root, "assets\\map_tiles\\forest", "thick_grass.png"))
+
+    def get_map_tile(self, key):
+        return self.id_texture_map[key]
