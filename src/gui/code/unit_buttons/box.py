5c7ff54e497d3874e6d9a48a22d1d5ca89125811
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2021-05-19 12:36:54 +0800

    Revert "Gui"

--- src/gui/code/unit_buttons/box.py
+++
@@ -1,9 +0,0 @@
-class Box:
-    def __init__(self, xpos, ypos, xlength, ylength):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.xlength = xlength
-        self.ylength = ylength
-
-    def check_inside(self, x, y):
-        return (self.xpos <= x <= self.xpos + self.xlength) and (self.ypos <= y <= self.ypos + self.ylength)
